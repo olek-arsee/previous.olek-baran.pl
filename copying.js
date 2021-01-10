281bbd95318dfe48d8aaedf77d35551f47b73604
olek-arsee <74045117+olek-arsee@users.noreply.github.com>
2021-01-10 20:19:04 +0100

    All files have been added
    The following files have been added: html, css, js, images, server file

---
+++ copying.js
@@ -0,0 +1,9 @@
+function copyToClipboard(text) {
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+    window.alert("E-Mail to contact with Olek from Arsee Projects has been copied to the clipboard.");
+}
